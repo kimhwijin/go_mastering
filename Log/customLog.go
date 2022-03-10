7f3e2d41d23c7d555c827f52c2b2b4021449dd18
kimhwijin <rlagnlwls0160@gmail.com>
2022-03-10 13:30:09 +0900

    custom file logging

---
+++ Log/customLog.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	LOGFILE, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	LOGFILE += "/GoMastering/go_mastering/Log/log/main.log"
+
+	f, err := os.OpenFile(LOGFILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer f.Close()
+
+	//Lstd : Last date, Last time 로그에 현재 날짜와 시작을 같이 저장한다.
+	cLog := log.New(f, "customLog", log.LstdFlags)
+	cLog.SetFlags(log.LstdFlags)
+	cLog.Println("Hello Log!")
+	cLog.Println("Another Log entry!")
+	//customLog2022/03/10 13:29:03 Hello Log!
+	//customLog2022/03/10 13:29:03 Another Log entry!
+}
